98e5e65c66fe4585506ad6b83d155aecd4a43a27
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-01-06 18:51:25 +0100

    fix time stretching

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -9,6 +9,8 @@
   const decimalFormat = new Intl.NumberFormat('en-IN', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
   const intFormat = new Intl.NumberFormat('en-IN', { maximumFractionDigits: 0, signDisplay: 'exceptZero' })
 
+  const maxFreq = Math.pow(2,6)-1
+
   let el;
   let scene = null
   let snap = false
@@ -16,17 +18,17 @@
   let freq = 0
   let phase = 0
   let amplitude = 1
-  let shape = 'constant'
+  let shape = 'rect'
   let timeShift = 0
-  let timeStretch = 1
+  let timeStretch = 0
   const samples = 512
 
   const shapes = {
   	constant: (x) => 1,
   	dirac: (x) => x==0 ? 1 : 0,
-  	rect: (x) => Math.abs(x*4)<1 ? 1 : 0,
+  	rect: (x) => Math.abs(x*Math.PI*8)<=1 ? 1 : 0,
   	sinc: (x) => !isFinite(x)?0:x==0?1:Math.sin(Math.PI*2*x*4)/(Math.PI*2*x*4),
-  	gauss: (x) => !isFinite(x)?0:Math.exp(-x*x*4),
+  	gauss: (x) => !isFinite(x)?0:Math.exp(-x*x*16*Math.PI*Math.PI),
   	sha: (x) => x%0.125==0 ? 1 : 0,
   }
 
@@ -39,11 +41,17 @@
   	'sha': 'sha',
   }
 
+  function swapShape(evt) {
+  	evt.preventDefault()
+  	shape = transformPairs[shape]
+  }
+
+  $: timeStetchExp = Math.pow(2,timeStretch)
   $: if(scene) {
   	scene.setFraction(fraction)
-  	scene.setSignal(Array(samples).fill(amplitude).map((a,i) => [a, (i/samples-0.5)]).map(([ampl,t]) => [ampl*shapes[shape](timeStretch*t-timeShift/samples)* Math.cos(Math.PI*2*(freq*2*t+phase/360)), ampl*shapes[shape](timeStretch*t-timeShift/samples)*Math.sin(Math.PI*2*(freq*2*t+phase/360))]))
+  	scene.setSignal(Array(samples).fill(amplitude).map((a,i) => [a, (i/samples-0.5)]).map(([ampl,t]) => [ampl*shapes[shape](timeStetchExp*(t-timeShift/samples))* Math.cos(Math.PI*2*(freq*2*t+phase/360)), ampl*shapes[shape](timeStetchExp*(t-timeShift/samples))*Math.sin(Math.PI*2*(freq*2*t+phase/360))]))
   	
-  	scene.setSpectrum(Array(samples).fill(amplitude).map((a,i) => [a, ((i-freq)/samples-0.5)]).map(([ampl, i]) => [ampl*shapes[transformPairs[shape]](i&&i/timeStretch)*Math.cos(Math.PI*2*(phase/360-2*i*timeShift)),ampl*shapes[transformPairs[shape]](i&&i/timeStretch)*Math.sin(Math.PI*2*(phase/360-2*i*timeShift))]))
+  	scene.setSpectrum(Array(samples).fill(amplitude).map((a,i) => [a, ((i-freq)/samples-0.5)]).map(([ampl, i]) => [ampl*shapes[transformPairs[shape]](i&&i/timeStetchExp)*Math.cos(Math.PI*2*(phase/360-2*i*timeShift)),ampl*shapes[transformPairs[shape]](i&&i/timeStetchExp)*Math.sin(Math.PI*2*(phase/360-2*i*timeShift))]))
   }
 
   onMount(() => {
@@ -110,20 +118,21 @@
 		<fieldset>
 			<legend style="white-space: nowrap;">Controls /<label><input type="checkbox" bind:checked={snap}>Snap</label></legend>
 
-			<label>Shape: {transformPairs[shape]} 
+			<label>Shape:<br>
 				<select bind:value={shape}>
 					{#each Object.keys(shapes) as shape}
 				<option value={shape}>{shape}</option>
 					{/each}
-				</select><br>
+				</select></label>
+				<output style="text-decoration: underline; cursor: pointer;" on:click={swapShape}>swap</output> 
+				<br>
 
 				<label>Time Shift: <output>{intFormat.format(timeShift)}</output>
-					<input type="range" min="-15" max="15" step="1" bind:value={timeShift} name="">
+					<input type="range" min="-{maxFreq}" max="{maxFreq}" step="1" bind:value={timeShift} name="">
 				</label>
 				<label>Time Stretch: <output>{intFormat.format(timeStretch)}</output>
-					<input type="range" min="0" max="15" step="0.125" bind:value={timeStretch} name="">
+					<input type="range" min="-3" max="3" step="1" bind:value={timeStretch} name="">
 				</label>
-		</label>
 
 			<hr>
 
@@ -131,7 +140,7 @@
 				<input list={snap?"ampl-list":null} type="range" min="0" max="2" step="0.01" bind:value={amplitude} name=""></label>
 			<label>Frequency:  <output>{intFormat.format(freq)}</output> 
 
-				<input type="range" min="-15" max="15" step="1" bind:value={freq} name=""></label>
+				<input type="range" min="-{maxFreq}" max="{maxFreq}" step="1" bind:value={freq} name=""></label>
 			<label>Phase: <output>{intFormat.format(phase)}°</output> 
 
 				<input list={snap?"phase-list":null} type="range" min="-180" max="180" step="5" bind:value={phase} name=""></label>
