7b9a5c648c0e3c29b42175881a560f3bae6fc1b8
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-01-19 13:40:02 +0100

    fractional fft almost finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(iter_array_chunks)]
+
 mod utils;
 
 use rustfft::Fft;
@@ -17,6 +19,10 @@ pub struct Signal {
     time: Vec<Complex<f32>>,
     freq: Vec<Complex<f32>>,
     frac: Vec<Complex<f32>>,
+
+    f1: Vec<Complex<f32>>,
+    f0c: Vec<Complex<f32>>,
+    f1c: Vec<Complex<f32>>,
 }
 
 
@@ -41,6 +47,8 @@ fn do_fft(fft: &Arc<dyn Fft<f32>>, source: &Vec<Complex<f32>>, mut target: &mut 
 #[wasm_bindgen]
 impl Signal {
     pub fn new(length: usize) -> Self {
+        utils::set_panic_hook();
+
         let mut planner = FftPlanner::new();
         let fft = planner.plan_fft_forward(length);
 
@@ -49,7 +57,11 @@ impl Signal {
         let frac = vec![Complex::default();length];
         fft.process(&mut freq);
 
-        Self { fft, time, freq, frac }
+        Self { fft, time, freq, frac,
+            f1: Default::default(),
+            f0c: Default::default(),
+            f1c: Default::default(),
+        }
     }
 
     pub fn get_time(&self) -> *const Complex<f32> {
@@ -78,8 +90,8 @@ impl Signal {
 
 
     pub fn update_frac(&mut self, fraction: f32) {
-        let N = self.time.len();
-        let fN = N as f32;
+        let n = self.time.len();
+        let f_n = n as f32;
         // a = a % 4;
 
         let mut a = fraction.rem_euclid(4.0);
@@ -111,75 +123,71 @@ impl Signal {
             }
            
             let alpha = a * PI / 2.0;
-            let s = PI / (fN + 1.0) / alpha.sin() / 4.0;
-            let t = PI / (fN + 1.0) * (alpha / 2.0).tan() / 4.0;
+            let s = PI / (f_n + 1.0) / alpha.sin() / 4.0;
+            let t = PI / (f_n + 1.0) * (alpha / 2.0).tan() / 4.0;
             let cs = Complex::<f32>::new(0.0, -1.0 * (1.0 - a) * PI / 4.0).exp() / (s/PI).sqrt();
 
-            let sincArray = (0..(2 * N - 2)).map(|i| -(2.0 * fN - 3.0) + 2.0 * i as f32).map(|x| sinc(x) * 0.5).collect::<Vec<_>>();
+            let sinc_iter = (0..(2 * n - 2)).map(|i| -(2.0 * f_n - 3.0) + 2.0 * i as f32).map(|x| sinc(x) * 0.5);
+            let sinc_len = 2 * n - 2;
 
-            let f1 = fast_conv(&self.frac, &sincArray);
-            ifft(f1);
-            f1.reverse();
-            f1.slice(N, 2 * N - 1);
-
-
-            let chrpA = (0..(2 * N - 1)).map(|i| -fN + 1.0 + i as f32).map(|x| Complex::<f32>::new(0.0, -1.0 * t * x*x).exp());
-            let chrpB = (0..(4 * N - 1)).map(|i| -(2.0 * fN - 1.0) + i as f32).map(|x| Complex::<f32>::new(0.0, 1.0 * s * x*x).exp());
-
-            let l0 = arrayMod(chrpA, 2, 0);
-            let l1 = arrayMod(chrpA, 2, 1);
-            let e1 = arrayMod(chrpB, 2, 0);
-            let e0 = arrayMod(chrpB, 2, 1);
+            fast_conv(&self.fft, self.frac.len(), sinc_len, self.frac.iter().cloned(), sinc_iter, &mut self.f1);
+            //ifft(f1);
+            //f1.reverse();
+            self.fft.process(&mut self.f1);
+            let f1_slice = &self.f1[n..(2 * n - 1)];
 
 
-            let f0m = complZipArrays(self.frac, l0, complMul);
-            let f1m = complZipArrays(f1, l1, complMul);
-            let f0c = fast_conv(f0m, e0);
-            let f1c = fast_conv(f1m, e1);
+            let chirp_a = (0..(2 * n - 1)).map(|i| -f_n + 1.0 + i as f32).map(|x| Complex::<f32>::new(0.0, -1.0 * t * x*x).exp());
+            let chirp_b = (0..(4 * n - 1)).map(|i| -(2.0 * f_n - 1.0) + i as f32).map(|x| Complex::<f32>::new(0.0, 1.0 * s * x*x).exp());
 
-            let h0 = complZipArrays(f0c, f1c, complAdd);
-            ifft(h0);
+            let (l0, l1) : (Vec<_>, Vec<_>) = chirp_a.array_chunks().map(|[a,b]| (a,b)).unzip();
+            let (e0, e1) : (Vec<_>, Vec<_>) = chirp_b.array_chunks().map(|[a,b]| (a,b)).unzip();
 
-            for (i, l) in l0.enumerate() {
-                self.frac[i] = complMul(cs, l, h0[N + i]) * f32::sqrt(fN)
+
+            let f0m_iter = self.frac.iter().zip(l0.iter()).map(|(a,b)| a*b);
+            let f1m_iter = f1_slice.iter().zip(l1).map(|(a,b)| a*b);
+
+            fast_conv(&self.fft, self.frac.len(), e0.len(), f0m_iter, e0.iter().cloned(), &mut self.f0c);
+            fast_conv(&self.fft, f1_slice.len(), e1.len(), f1m_iter, e1.iter().cloned(), &mut self.f1c);
+
+            let mut h0 : Vec<_> = self.f0c.iter().zip(self.f1c.iter()).map(|(a,b)| a+b).collect();
+            self.fft.process(&mut h0);
+            h0.reverse();
+
+            for (i, l) in l0.iter().enumerate() {
+                self.frac[i] = cs * l * h0[n + i] * f32::sqrt(f_n)
             }
         }
-
-
-        // const alpha = a * Math.PI / 2;
-        // const s = Math.PI / (N + 1) / Math.sin(alpha) / 4;
-        // const t = Math.PI / (N + 1) * Math.tan(alpha / 2) / 4;
-        // const Cs = complScale(complExp(compl(0, -1 * (1 - a) * Math.PI / 4)), Math.sqrt(s / Math.PI));
-
-        // const sincArry = Array.from({length: 2 * N - 2}, (_, i) => -(2 * N - 3) + 2 * i).map(sinc).map(x => complScale(x, 1/2))
-
-        // const f1 = ifft(fconv(f0, sincArry)).reverse().slice(N, 2 * N - 1);
-        
-
-        //---
-
-        // const chrpA = Array.from({length: 2 * N - 1}, (_, i) => complExp(compl(0, -1 * t * Math.pow(-N + 1 + i, 2))));
-        // const l0 = arrayMod(chrpA, 2, 0)
-        // const l1 = arrayMod(chrpA, 2, 1)
-        // const f0m = complZipArrays(f0, l0, complMul);
-        // const f1m = complZipArrays(f1, l1, complMul);
-        
-        // const chrpB = Array.from({length: 4 * N - 1}, (_, i) => complExp(compl(0, 1 * s * Math.pow(-(2 * N - 1) + i, 2))));
-        // const e1 = arrayMod(chrpB, 2, 0);
-        // const e0 = arrayMod(chrpB, 2, 1);
-        // const f0c = fconv(f0m, e0);
-        // const f1c = fconv(f1m, e1);
-        // const h0 = ifft(complZipArrays(f0c, f1c, complAdd));
-
-        // return l0.map((l, i) => complMul(Cs, l, h0[N + i])).map(x => complScale(x, Math.sqrt(N)))
     }
 }
 
 
 fn sinc(x: f32) -> Complex<f32> {
-    Complex::default()
+    Complex::new(f32::sin(PI * x) / (PI * x), 0.0)
 }
 
-fn fast_conv(a: &Vec<Complex<f32>>, b: &Vec<Complex<f32>>) {
+fn fast_conv(fft: &Arc<dyn Fft<f32>>, a_size: usize, b_size: usize, a: impl Iterator<Item=Complex<f32>>, b: impl Iterator<Item=Complex<f32>>, into: &mut [Complex<f32>]) {
+    let n = a_size + b_size - 1;
+    let p = 2 << f32::ceil(f32::log2(n as f32)) as usize;
+    let mut padded_a = padded(a, p);
+    let mut padded_b = padded(b, p);
+    fft.process(&mut padded_a);
+    fft.process(&mut padded_b);
 
+    iter_into_slice(padded_a.iter().zip(padded_b.iter()).map(|(a,b)| a*b), into)
+}
+
+fn padded(orig: impl Iterator<Item=Complex<f32>>, target_length: usize) -> Vec<Complex<f32>> {
+    let mut padded = vec![Complex::default();target_length];
+
+    iter_into_slice(orig, &mut padded);
+
+    padded
+}
+
+
+fn iter_into_slice<T>(iter: impl Iterator<Item=T>, slice: &mut[T]) {
+    for (old, it) in slice.iter_mut().zip(iter) {
+        *old = it
+    }
 }
